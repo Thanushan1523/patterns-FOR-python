199ee17d0627043365182f9fe4720900e8016620
Thanushan1523 <thanushan1523@gmail.com>
2023-10-22 15:26:25 -0400

    square patterns

--- python.py
+++ python.py
@@ -1,3 +1,18 @@
-print("hi")
+# print("hi")
 
-print ("hellow")
+# print ("hellow")
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print("*" )
+ 
+
+# for i in range (n):
+#     print("*" ,end="")
+
+# for i in range (n):
+#     print("*" ,end=" ")
+
+
+n=int(input("enter number of rows:"))
+for i in range (n):
+    print("* " *n )

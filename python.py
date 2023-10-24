b6792aef507e2749d8e55db9163eeed7f1ced5eb
Thanushan1523 <thanushan1523@gmail.com>
2023-10-24 09:10:19 -0400

    reverse tri angle

--- python.py
+++ python.py
@@ -39,3 +39,8 @@
 # n=int(input("enter number of rows:"))
 # for i in range (n):
 #     print((chr(65+i)+" ") *(i+1) )
+
+
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print("* " * (n-i))

70065b3d7b379b595e2606af200d05ca63452e3a
Thanushan1523 <thanushan1523@gmail.com>
2023-10-25 07:32:10 -0400

    pyramid num pattern with fixed digit in each row

--- python.py
+++ python.py
@@ -44,3 +44,17 @@
 # n=int(input("enter number of rows:"))
 # for i in range (n):
 #     print("* " * (n-i))
+
+
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     for j in range (n-i):
+#         print((j+1) ,end="  ")
+#     print() 
+
+
+# pyramid number patteren with fixed num in rows
+n=int(input("enter number of rows:"))
+for i in range (n):
+    print(" "*(n-i-1) + (str(i+1)+" ") *(i+1))
+    

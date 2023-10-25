b5e797c69265e68d7c549b7e354101279b0b213e
Thanushan1523 <thanushan1523@gmail.com>
2023-10-25 07:52:25 -0400

    PYRAMID PATTERN WITH ALPHABET SYMBOLS IN REVERESE OF DICTONARY

--- python.py
+++ python.py
@@ -54,7 +54,26 @@
 
 
 # pyramid number patteren with fixed num in rows
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print(" "*(n-i-1) + (str(i+1)+" ") *(i+1))
+
+
+# DOUBT I HAVE TO MAKE A NESTED LOOP FOR ABOVE 
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print(" "*(n-i-1), end='') 
+#     for j in range (i+1):
+#            print(n-j,end=" ") 
+
+#     print()   
+
+
+# PYRAMID PATTERN WITH ALPHABET SYMBOLS IN REVERESE OF DICTONARY
+
 n=int(input("enter number of rows:"))
 for i in range (n):
-    print(" "*(n-i-1) + (str(i+1)+" ") *(i+1))
-    
+    print(" "*(n-i-1), end='') 
+    for j in range (i+1):
+           print(chr(64+n-j),end=" ")
+    print()    

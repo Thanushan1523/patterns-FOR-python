a84ecf6e338263d942248e40341d7ed60714fff6
Thanushan1523 <thanushan1523@gmail.com>
2023-10-26 07:33:04 -0400

    iverted pyramid pattern with alphabet symbol in dictonary order in every row

--- python.py
+++ python.py
@@ -99,9 +99,39 @@
 #            print(chr(65+j),end=" ")
 #     print()    
 
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print(" " * i,end=" " )
+#     for j in range (n-i):
+#            print(chr(65+j),end=" ")
+#     print()    
+
+
+
+# wrong ans but differnt pattern
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+   
+#     for j in range (i+1):
+#            print(" "*(n-1-i),"*" *(i+1))
+#     print()    
+# for i in range (n-1):
+   
+#     for j in range (n-i-1):
+#            print(" "*(i+1), "*"*(n-1-i))
+#     print()
+
+
 n=int(input("enter number of rows:"))
 for i in range (n):
-    print(" " * i,end=" " )
-    for j in range (n-i):
-           print(chr(65+j),end=" ")
-    print()    
+ print(" "*(n-1-i),"*" *(i+1))
+  
+for i in range (n-1):
+       print(" "*(i+1), "*"*(n-1-i))
+ 
+n=int(input("enter number of rows:"))
+for i in range (n):
+ print(" "*(n-1-i),"* " *(i+1))
+  
+for i in range (n-1):
+       print(" "*(i+1), "* "*(n-1-i))

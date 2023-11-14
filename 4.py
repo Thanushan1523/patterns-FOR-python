6794e80630695b91307d7e1ba7c94a2071ea4e3d
Thanushan1523 <thanushan1523@gmail.com>
2023-11-13 20:50:07 -0500

    4.py

--- 4.py
+++ 4.py
@@ -1,3 +1,22 @@
+
+#  1 
+#  2 2
+# 3 3 3
+
+
+# n=int(input("enter number of rows:"))
+# for i in range (n):
+#     print(" "*(n-i-1) + (str(i+1)+" ") *(i+1))
+
+
+# pyramid number patteren with fixed num in rows
+
+
+# DOUBT I HAVE TO MAKE A NESTED LOOP FOR ABOVE 
 n=int(input("enter number of rows:"))
 for i in range (n):
-    print(" "*(n-i-1) + (str(i+1)+" ") *(i+1))
+    print(" "*(n-i-1), end='') 
+    for j in range (i+1):
+           print(n-j,end=" ") 
+
+    print()   

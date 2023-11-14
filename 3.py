7f6271619e4757408420d448291e2770cdb801a0
Thanushan1523 <thanushan1523@gmail.com>
2023-11-13 20:47:02 -0500

    4.py

--- 3.py
+++ 3.py
@@ -2,6 +2,19 @@
 # * * *
 # * *
 # *
+
+
+# 1  2  3  
+# 1  2
+# 1
+
 n=int(input("enter number of rows:"))
 for i in range (n):
     print("* " * (n-i))
+
+
+n=int(input("enter number of rows:"))
+for i in range (n):
+    for j in range (n-i):
+        print((j+1) ,end="  ")
+    print() 
---
+++ function.py
@@ -0,0 +1,13 @@
+def great(name):
+    gr="hi  " + name
+    return gr
+a=great("kbfb")
+
+print(a)
+
+def great(name):
+    gr="hi  " + name
+    return gr
+a=great("kbfb")
+
+print(a)
